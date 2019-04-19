c9641d99ed7f0db00c0666c629e07de005a4ff37
Madeline Montgomery <mdlnmontgomery@gmail.com>
2019-04-19 02:08:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var greeting = "Hello, ";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
@@ -6,4 +6,4 @@ var greetSpecialGuest = "greeting" + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `Let\'s talk about ${topic}`;
+var conversation = `Let\'s talk about ${topic}.`;
